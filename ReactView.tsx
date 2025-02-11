5a0b42ead7da6e51d70417a5df36304f6fadfdd4
shin5884 <shin5884.kt@gmail.com>
2025-02-11 10:06:54 +0900

    feat: neko画像を表示できるように

--- ReactView.tsx
+++ ReactView.tsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+
 export const ReactView = () => {
-    return <h4>Hello, Nekochan!</h4>;
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchNekoImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);
+
+    return <div>{loading || <img src={imageUrl}/>}</div>;
 }
+
+type Image = {
+    url: string;
+};
+const fetchNekoImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    return images[0];
+};
